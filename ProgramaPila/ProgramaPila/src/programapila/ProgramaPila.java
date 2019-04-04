936b81facd50be6b2580092cdcf2978b35d1d4b5
rebeefal <rebeefal@gmail.com>
2019-04-04 17:13:24 -0500

    crear gui nuevamente

--- ProgramaPila/ProgramaPila/src/programapila/ProgramaPila.java
+++ ProgramaPila/ProgramaPila/src/programapila/ProgramaPila.java
@@ -1,13 +1,7 @@
 
 package programapila;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+
 import javax.swing.JFrame;
 
 
@@ -18,56 +12,14 @@ public class ProgramaPila extends JFrame {
     }  
     
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {                                            
-        // TODO add your handling code here:
-    }                                           
-
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {                                            
-        // TODO add your handling code here:
-    }                                           
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
-    }                                        
-
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {                                            
-        // TODO add your handling code here:
-    }                                           
-
-    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // TODO add your handling code here:
-    }                                        
-
-
-    // Variables declaration - do not modify                     
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    // End of variables declaration    
-
+ 
     
     
      public static void main(String[] args) {
-         
         ProgramaPila inter = new ProgramaPila();
         inter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         inter.setSize(800, 580);
-        inter.setVisible(true);
-         
-         
-      
+        inter.setVisible(true);   
     }
     
     
